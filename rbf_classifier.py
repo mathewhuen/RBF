59736268f499d66593d3553236178bff17bb76e9
mathewhuen <mathewhe@gmail.com>
2020-05-15 00:00:25 +0900

    Added linear kernel and updated main module
    Changed distance calculation to be handled in .forward rather than in individual kernel functions.

--- rbf_classifier.py
+++ rbf_classifier.py
@@ -4,11 +4,11 @@ import torch.functional as F
 
 class RBF(nn.Module):
     """
-    A pytorch RBF layer (a more general version of https://github.com/JeremyLinux/PyTorch-Radial-Basis-Function-Layer/blob/master/Torch%20RBF/torch_rbf.py)
+    A pytorch RBF module (a more general version of https://github.com/JeremyLinux/PyTorch-Radial-Basis-Function-Layer/blob/master/Torch%20RBF/torch_rbf.py with rejection according to output of rbf layer)
     Accepts any input shape.
     
     """
-    def __init__(self, d_in, d_out, k, basis_fnc='gaussian', init_args=None, classify=True):#d_in and d_out must be tuples with first dimension corresponding to the squeezed shape of a single input to batch index
+    def __init__(self, d_in, k, basis_fnc='gaussian', init_args=None):#d_in and d_out must be tuples with first dimension corresponding to the squeezed shape of a single input to batch index
         """
         
         Arguments
@@ -30,20 +30,34 @@ class RBF(nn.Module):
             
             This dictionary must contain keys for kernel 'centers' and any other parameters used in the kernel function. Each key should map to another dictionary with keys 'fnc' and 'args' corresponding to an initialization function (eg, nn.init.normal_) and a kwargs dict for said initialization function, respectively.
         
-        classifiy: bool, optional
+        classifiy: bool, optional (currently not implemented and will probably remove since it only makes sense with rbf kernels that scale positively with distance)
             Indicates whether or not a dense layer with softmax activation should be appended to the results of the rbf kernel. Default is True
             
             If classify is set to False, d_out is ignored, and the output is of the shape (batch size, k).
+            
+        
+        Usage
+        -----
+        There are two recommended uses for this layer:
+        1) Shallow RBF Network - accepting either input directly from the raw feature space or a simple model
+            A) input -> rbf layer -> dense layer -> softmax
+            B) input -> shallow model -> rbf layer -> dense layer -> softmax
+            
+        2) Deep RBF Network - accepting input from a deep model.
+            A) input -> deep model -> rbf layer (k = number of classes) -> softmax
+        
+        See the associated readme file for more explanation and examples: https://github.com/mathewhuen/RBF
         """
         super(RBF, self).__init__()
         self.d_in = d_in if type(d_in)==tuple else (d_in,)
-        self.d_out = d_out
+#         self.d_out = d_out
         self.k = k
-        self.classify = classify
+#         self.classify = classify
         
         basis_fncs = {
             'gaussian':gaussian,
-            'inverse_quadratic':inverse_quadratic
+            'inverse_quadratic':inverse_quadratic,
+            'linear':linear
         }
         
         basis_fnc_params = {
@@ -55,6 +69,9 @@ class RBF(nn.Module):
                 }
             },
             'inverse_quadratic' : {
+            },
+            'linear' : {
+                
             }
         }
         
@@ -66,6 +83,7 @@ class RBF(nn.Module):
         params.update([('centers', nn.Parameter(torch.Tensor(1, *self.d_in, k))), 
                        *[(key, nn.Parameter(torch.Tensor(*(basis_fnc_params[basis_fnc][key]['dims'])))) for key in basis_fnc_params[basis_fnc].keys()]])
         self.params = nn.ParameterDict(params)
+        self.threshold = nn.Parameter()
         
         if(init_args==None):
             init_args = {}
@@ -73,48 +91,41 @@ class RBF(nn.Module):
                        *[(key, {'fnc':basis_fnc_params[basis_fnc][key]['fnc'], 'args':basis_fnc_params[basis_fnc][key]['args']}) for key in basis_fnc_params[basis_fnc].keys()]])
         self._initialize_params(init_args)
         
-        if(self.classify):
-            self.net = nn.Sequential()#later set flag for this...I might want just output of rbf layer
-            self.net.add_module('rbf_weights', nn.Linear(self.k, self.d_out))
-            self.net.add_module('softmax', nn.Softmax(dim=1))#later add a threshold to this
+        self.input_size_error = 'Actual input dimension {ain} does not match specified input dimension {spin} for all dim>=1. Please note that RBF does not yet support variable shaped inputs. If you are working with variable shaped convolutional inputs, consider using global max pooling to force the dimension to the number of feature maps or another dimension forcing technique like Spatial Pyramid Pooling.'
 
     def _initialize_params(self, kwargs):
         for param in self.params.keys():
             kwargs[param]['fnc'](self.params[param], **(kwargs[param]['args']))
             
-    def forward(self, x):
+    def forward(self, x, p=2):
         if(len(x.shape)==1):
             x = x.reshape(1,x.shape[0])
         if(self.d_in!=x[0].shape):
-            error_str = 'Actual input dimension ({ain}) does not match specified input dimension ({spin}) for all dim>=1. Please note that RBF does not yet support variable shaped inputs. If you are working with variable shaped convolutional inputs, consider using global max pooling to force the dimension to the number of feature maps or another dimension forcing technique like Spatial Pyramid Pooling.'.format(ain=x.shape, spin=(1, *self.d_in))
+            error_str = self.input_size_error.format(ain=x.shape, spin=(1, *self.d_in))
             raise TypeError(error_str)
-        dim_mod = [1]*(len(self.d_in)+1)
-#         print(dim_mod)
-#         dist = self.params['centers']-x.reshape(-1, *dim_mod).repeat(*dim_mod,self.k)#use squeeze and unsqueeze and expand
-        dist = self.params['centers']-x.reshape(*x.shape, 1).repeat(*dim_mod,self.k)#use squeeze and unsqueeze and expand
-#         print(self.params['centers'].shape, x.shape)
         
+        dim_mod = [1]*(len(self.d_in)+1)
+        dist = self.params['centers']-x.reshape(*x.shape, 1).repeat(*dim_mod,self.k)#use squeeze and unsqueeze and expand
+        dist = dist.pow(p).sum(tuple(range(1,len(dist.shape)-1)))#.pow(1/p)
         r = self.basis_fnc(dist, {key:self.params[key] for key in self.params.keys() if key!='centers'})
         
-        y = self.net(r) if self.classify else r
-        
-        return(y)
+        return(r)
     
     
     
 def gaussian(dist, params, p=2):
     """
     """
-    n = len(dist.shape)
     n_obs = dist.shape[0]
     gamma = params['gamma']
-    dist = dist.pow(p).sum(tuple(range(1,n-1))) * gamma.unsqueeze(0).expand(n_obs, -1)
+    dist = dist * gamma.unsqueeze(0).expand(n_obs, -1)
     phi = torch.exp(-dist)
     return(phi)
     
 def inverse_quadratic(dist, params, p=2):
-    n = len(dist.shape)
-    n_obs = dist.shape[0]
-    dist = dist.pow(p).sum(tuple(range(1,n-1)))
     phi = torch.ones_like(dist) / (torch.ones_like(dist) + dist)
+    return phi
+
+def linear(dist, params, p=2):
+    phi = dist.pow(1/p)
     return phi
